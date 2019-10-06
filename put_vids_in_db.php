d6c6f92d717883585493448a3391fe43958454dd
loicbcn <loicbcn@users.noreply.github.com>
2019-10-06 18:59:41 +0200

    write in db

--- put_vids_in_db.php
+++ put_vids_in_db.php
@@ -14,8 +14,13 @@ ini_set("xdebug.var_display_max_data", 1000);
 ini_set("xdebug.var_display_max_depth", 5);
 
 include('../lib/functions.php');
+$PDO = getpdo();
+$insertsql = 'INSERT INTO myvids (filename, friendly, extension) values(:filename, :friendly, :extension)';
+$q = $PDO->prepare($insertsql);
 
-$data_path = 'D:/donnees/perso/videos/';
+
+
+$data_path = 'D:/yt_vids/Takeout/YouTube/videos/';
 $vids = glob("$data_path*{.avi,.mp4}",GLOB_BRACE);
 $counter = 0;
 
@@ -24,11 +29,13 @@ $cleans = [];
 $doubles = [];
 foreach ($vids as $v) {
   $pathinfo = pathinfo($v);
+
   //var_dump($pathinfo);
   $original_name = $pathinfo['filename'];
   $ext = $pathinfo['extension'];
   $clean_name = friendly_url(utf8_encode($original_name));  
   
+  $q->execute([':filename' => utf8_encode($original_name), ':friendly' => friendly_url(utf8_encode($original_name)), 'extension' => $pathinfo['extension']]);
   $allv[] = ['original' => $original_name, 'convert' => $clean_name];
   
   if (in_array($clean_name, $cleans)) {
@@ -40,3 +47,13 @@ foreach ($vids as $v) {
 
 var_dump($doubles);
 var_dump($allv);
+
+
+function getpdo(){
+
+    $PDO = new PDO('mysql:host=localhost;dbname=recup_vids','root','root');
+    $PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+    $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    return $PDO;
+
+}
